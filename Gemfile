12c25ebf7de33428eade1386ef28304ce2172480
Dmitri Akatov <akatov@gmail.com>
2014-07-26 01:30:30 +0100

    allos the app API being accessed from different domain

--- Gemfile
+++ Gemfile
@@ -39,3 +39,5 @@ gem 'spring',        group: :development
 # gem 'debugger', group: [:development, :test]
 
 gem 'active_model_serializers'
+
+gem 'rack-cors'
--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,16 @@ module Jesse
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post]
+      end
+    end
+
+    # Disable request forgery protection in test environment.
+    config.action_controller.allow_forgery_protection = false
+
   end
 end
